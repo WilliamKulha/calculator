dae88c5e7d944f22ead38dc6abf09a83f8e90a5c
William Kulha <kulha.william@gmail.com>
2018-09-04 16:43:35 -0400

    Add functions that will subtract, multiply, and divide two numbers to the calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,48 @@ var add = function(number1, number2) {
 return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number"));
+var number1 = parseInt(prompt("Enter the first number you'd like to add to"));
 var number2 = parseInt(prompt("Enter another number"));
-var result = add(number1, number2);
+//var result = add(number1, number2);
 
-alert(result);
+//alert(result);
+
+alert(add(number1, number2));
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter the number you'd like to subtract from"));
+var number2 = parseInt(prompt("Enter another number"));
+//var result = subtract(number1, number2);
+
+//alert(result);
+
+alert(subtract(number1, number2));
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter the first number you'd like to multiply"));
+var number2 = parseInt(prompt("Enter the other number you'd like to multiply"));
+
+//var result = multiply(number1, number2);
+
+//alert(result);
+
+alert(multiply(number1, number2));
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1= parseInt(prompt("Enter the number you'd like to divide"));
+var number2=parseInt(prompt("Enter the number you'd like to divide it by"));
+
+//var result= divide(number1, number2);
+
+//alert(result);
+
+alert(divide(number1, number2));
