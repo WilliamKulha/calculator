c2860b0fc542fff2011b5b6672a59a0ed6003950
William Kulha <kulha.william@gmail.com>
2018-09-04 16:24:11 -0400

    Create new variable called result that will clean up the line of code where the alert is called.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ return number1 + number2;
 };
 
 var number1 = parseInt(prompt("Enter a number"));
-
 var number2 = parseInt(prompt("Enter another number"));
+var result = add(number1, number2);
 
-alert(add(number1, number2));
+alert(result);
