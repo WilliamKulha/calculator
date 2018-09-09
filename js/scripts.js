c4ba0cf35a3d83719782b2154ad2522191be40b3
William Kulha <kulha.william@gmail.com>
2018-09-09 16:23:03 -0400

    Add JS that adds the two inputted numbers and displays them in a div below the form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,25 @@
 var add = function(number1, number2) {
-return number1 + number2;
+  return number1 + number2;
 };
-
-var number1 = parseInt(prompt("Enter the first number you'd like to add to"));
-var number2 = parseInt(prompt("Enter another number"));
-//var result = add(number1, number2);
-
-//alert(result);
-
-alert(add(number1, number2));
 
 var subtract = function(number1, number2) {
   return number1 - number2;
 };
 
-var number1 = parseInt(prompt("Enter the number you'd like to subtract from"));
-var number2 = parseInt(prompt("Enter another number"));
-//var result = subtract(number1, number2);
-
-//alert(result);
-
-alert(subtract(number1, number2));
-
 var multiply = function(number1, number2) {
   return number1 * number2;
 };
-
-var number1 = parseInt(prompt("Enter the first number you'd like to multiply"));
-var number2 = parseInt(prompt("Enter the other number you'd like to multiply"));
-
-//var result = multiply(number1, number2);
-
-//alert(result);
-
-alert(multiply(number1, number2));
 
 var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1= parseInt(prompt("Enter the number you'd like to divide"));
-var number2=parseInt(prompt("Enter the number you'd like to divide it by"));
-
-//var result= divide(number1, number2);
-
-//alert(result);
-
-alert(divide(number1, number2));
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = (add(number1, number2));
+    $("div#output").text(result);
+  });
+});
